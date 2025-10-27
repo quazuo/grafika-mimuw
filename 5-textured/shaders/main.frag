112e958e94f7d70b0ec8f64858214cb82d2a41ad
quazuo <filipglebocki01@gmail.com>
2025-10-27 21:58:38 +0100

    comments and etc

--- 5-textured/shaders/main.frag
+++ 5-textured/shaders/main.frag
@@ -5,8 +5,7 @@ in vec2 tex_coords;
 out vec4 out_color;
 
 uniform sampler2D colorTexture;
-uniform sampler2D colorTexture2;
 
 void main() {
-    out_color = texture(colorTexture, tex_coords) + texture(colorTexture2, tex_coords);
+    out_color = texture(colorTexture, tex_coords);
 }
--- 5-textured/src/renderer.cpp
+++ 5-textured/src/renderer.cpp
@@ -225,13 +225,17 @@ void OpenGLRenderer::loadTextures() {
     }
 
     glGenTextures(1, &colorTextureID);
-    glActiveTexture(GL_TEXTURE0);
+    glActiveTexture(GL_TEXTURE0); // there are guaranteed 16 slots from GL_TEXTURE0 to GL_TEXTURE16; we put this texture in slot 0
     glBindTexture(GL_TEXTURE_2D, colorTextureID);
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    // border color used when we set either of the GL_TEXTURE_WRAP_* to GL_CLAMP_TO_BORDER
+    float borderColor[] = { 1.0f, 1.0f, 0.0f, 1.0f };
+    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
 
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
     glGenerateMipmap(GL_TEXTURE_2D);
